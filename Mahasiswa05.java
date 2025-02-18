499e2ab7e3b1d6a81fe1e29e33f8ec600ba2b5cf
oninonins <varelinobubadibako11@gmail.com>
2025-02-18 11:40:58 +0700

    new

---
+++ Mahasiswa05.java
@@ -0,0 +1,38 @@
+class Mahasiswa05 {
+    //atiribut
+    String nama;
+    String nim;
+    String kelas;
+    double ipk;
+
+    void tampilkanInformasi() {
+        System.out.println("Nama: " + nama);
+        System.out.println("NIM: " + nim);
+        System.out.println("Kelas: " + kelas);
+        System.out.println("IPK: " + ipk);
+    }
+
+    void ubahKelas(String kelasBaru) {
+        kelas = kelasBaru;
+    }
+    void updateIpk(double ipkBaru) {
+        if (ipkBaru >= 0.0 && ipkBaru <= 4.0) {
+            ipk = ipkBaru;
+        } else {
+            System.out.println("IPK tidak valid");
+        }
+    }
+
+    String nilaiKinerja () {
+        if (ipk >= 3.5) {
+            return "Kinerja Sangat Baik";
+        } else if (ipk >= 3.0) {
+            return "Kinerja baik";
+        } else if (ipk >= 2.0) {
+            return "kinerja cukup";
+        } else {
+            return "Kinerja kurang";
+        }
+    }
+    
+}
